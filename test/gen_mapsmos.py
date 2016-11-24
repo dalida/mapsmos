1355a4cab46a80193d7ce8847f247ca593c2fb3b
Lisa <lstilo@yahoo.com>
2016-11-24 15:58:18 +0100

    add logging

--- test/gen_mapsmos.py
+++ test/gen_mapsmos.py
@@ -11,10 +11,14 @@ import shutil
 import shlex
 from optparse import OptionParser
 import mapsmos
+import logging as log
 
 
 # get configurations file info
 # get dissPath, FTP arbo path
+
+# config logging
+log.basicConfig(format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=log.DEBUG)
 
 # retrieving args
 usage = 'usage : %prog [options]'
@@ -30,11 +34,13 @@ if options.diss_path:
   diss_path = str(options.diss_path.strip())
 else:
     print "Le chemin de l espace de dissemination est manquant"
+    log.info("Le chemin de l espace de dissemination est manquant")
     exit(1)
 
 # checking mapsmos executable
 if not os.path.exists('mapsmos.py'):
     print "L executable mapsmos.py n existe pas"
+    log.info("L executable mapsmos.py n existe pas")
     exit(1)
 
 # temporary directory
@@ -45,73 +51,69 @@ if not os.path.exists(tmpdir):
 # create dir MAP
 if not os.path.exists(diss_path):
   print "Erreur le chemin de dissemination " + diss_path + " n existe pas"
+  log.info("Erreur le chemin de dissemination " + diss_path + " n existe pas")
   exit(1)
 else:
   ftp_diss_path = diss_path + os.sep + "FTP"
   # checking ftp_diss_path
   if not os.path.exists(ftp_diss_path):
     print "Le repertoire de dissemination FTP " + ftp_diss_path + " n existe pas"
+    log.info("Le repertoire de dissemination FTP " + ftp_diss_path + " n existe pas")
     exit(1)
 
-  print "Nettoyage des cartes"
+  log.info("Nettoyage des cartes")
   map_diss_path = diss_path + os.sep + "MAP"
-  ## LM
-  print "diss_path : " + map_diss_path
+  log.debug("diss_path : " + map_diss_path)
 
   if not os.path.exists(map_diss_path):
       os.makedirs(diss_path + os.sep + "MAP")
   else:
     # loop through MAP arbo
 
-    # LM
-    print "LOOP through MAP tree"
+    log.info("LOOP through MAP tree")
 
     for root, directories, filenames in os.walk(map_diss_path):
       for filename in filenames:
         file_map_path = os.path.join(root,filename)
         ftp_dir = root.replace('/MAP/', '/FTP/', 1)
 
-        # LM
-        print "ftp_dir : " + ftp_dir
+        log.debug("ftp_dir : " + ftp_dir)
 
         ftp_file = None
         file_to_match = filename.split('.')[0]
 
-        # LM
-        print "file to match : " + file_to_match
+        log.debug("file to match : " + file_to_match)
 
         for loop_file in os.listdir(ftp_dir):
           if loop_file.startswith(file_to_match):
             ftp_file = loop_file
             break
         if ftp_file is None:
-          print "Le produit associee a la carte " + filename + " n existe pas"
+          log.info("Le produit associee a la carte " + filename + " n existe pas")
           os.remove(file_map_path)
           # removing empty parent file
           curr_dir = os.path.dirname(file_map_path)
           while curr_dir != map_diss_path:
             if os.path.exists(curr_dir) and len(os.listdir(curr_dir)) == 0:
               os.rmdir(curr_dir)
-              print "Repertoire vide supprime : " + curr_dir
+              log.info("Repertoire vide supprime : " + curr_dir)
               curr_dir = os.path.dirname(curr_dir)
             else:
               break
 
-  print "Generation des cartes"
+  log.info("Generation des cartes")
   mapsmos_conf = open("mapsmos.json")
   mapsmos_prd = json.load(mapsmos_conf)["META"].keys()
   mapsmos_conf.close()
 
   # loop through FTP arbo
   for root, directories, filenames in os.walk(ftp_diss_path):
-     # LM
-     print "root : " + root 
+     log.debug("root : " + root)
 
      # for directory in directories:
      # print os.path.join(root, directory)
      for filename in filenames:
-      # LM
-      print "filename : " + filename
+      log.debug("filename : " + filename)
 
       to_process = False
       if (filename.endswith('.tgz') or filename.endswith('.zip')
@@ -123,25 +125,22 @@ else:
             # check if map already exists
             map_dir = root.replace('/FTP/', '/MAP/', 1)
 
-            # LM
-            print "map_dir : " + map_dir
+            log.debug("map_dir : " + map_dir)
 
             map_file = None
             if os.path.exists(map_dir):
               file_to_match = filename.split('.')[0]
 
-              # LM
-              print "file_to_match : " + file_to_match
+              log.debug("file_to_match : " + file_to_match)
 
               for loop_file in os.listdir(map_dir):
                 if loop_file.startswith(file_to_match):
                   map_file = loop_file
-                  print "La carte associee au produit " + filename + " existe deja"
+                  log.info("La carte associee au produit " + filename + " existe deja")
                   break
 
             if map_file is None:
-              # LM
-              print "map_file is None..."
+              log.debug( "map_file is None...")
               if filename.endswith('.tgz'):
                 # untar
                 tar = tarfile.open(file_ftp_path)
@@ -167,13 +166,12 @@ else:
 
               if file_to_process:
 
-                # LM
-                print "file to process..."
+                log.debug("file to process...")
                 try:
                   mapsmos.mapsmos(file_to_process, map_dir)
-                  print "succes de la generation de la carte a partir du produit " + os.path.basename(file_to_process)
+                  log.info("succes de la generation de la carte a partir du produit " + os.path.basename(file_to_process))
                 except Exception as err:
-                  print ("echec de la generation de la carte a partir du produit " + os.path.basename(file_to_process) +
+                  log.info("echec de la generation de la carte a partir du produit " + os.path.basename(file_to_process) +
                          " avec le code erreur " + str(err))
                   
               # cleaning temporary files
@@ -181,7 +179,7 @@ else:
                 os.remove(os.path.join(tmpdir, file_to_clean))
               # stop the mapsmos_prd loop
               break
-print "Fin de la generation des cartes"
+log.info("Fin de la generation des cartes")
 # cleaning
 os.rmdir(tmpdir)
 exit(0)
